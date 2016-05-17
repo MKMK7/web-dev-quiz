395cb2cfd0cd9523bf0618aeec4b0ed93c90808a
MKMK7 <mohammad.karim@outlook.com>
2016-05-16 20:57:14 -0700

    updated script file

--- quiz.js
+++ quiz.js
@@ -1,12 +1,6 @@
-
-
 console.log("Hi there! This message was made with JavaScript!");
-
-
 var submitButton = document.getElementById("submit");
-
-submitButton.addEventListener("click", sayHello);
-
+submitButton.addEventListener("click",sayHello);
 function sayHello() {
   var messageBox = document.getElementById("message");
   messageBox.innerText = "Hello there, this is a message!";
